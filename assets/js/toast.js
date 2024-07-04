b60aeef3f1761123f5a50584ee1bf7293b8012f4
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-07-05 00:22:14 +0530

    toast-update-2.2.3

--- assets/js/toast.js
+++ assets/js/toast.js
@@ -5,5 +5,7 @@ window.onload = function() {
 function showToast() {
     var toast = document.getElementById("toast");
     toast.className = "show";
-    setTimeout(function() { toast.className = toast.className.replace("show", ""); }, 5000);
+    setTimeout(function() { 
+        toast.className = toast.className.replace("show"); 
+    }, 3000);
 }
