90b7aaa9cb290da69c1617ad5b86c5f73fa40455
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-07-05 16:13:20 +0530

    about-us-update-4.1

---
+++ assets/js/image-changer.js
@@ -0,0 +1,17 @@
+document.getElementById("target-me").open = true;
+const details = document.querySelectorAll("details");
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
+
+function ImgChanger(i) {
+    document.getElementById("outcomes-img").setAttribute("src", `assets/images/about-us-page/image-${i}.svg`);
+}
+
+  
