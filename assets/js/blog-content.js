63a0df7cedd36939d2b646dbcabb08479a4dc664
sreeharipdsy <sreehari.pdsy@gmail.com>
2024-08-15 19:29:09 +0530

    6.6.13

---
+++ assets/js/blog-content.js
@@ -0,0 +1,14 @@
+
+const blogs = [
+  {
+    id: 1,
+    image: "assets/images/courses-page/cuet-foundation.svg",
+    author: "Risha",
+    date: "14/08/2024",
+    heading: "Blog Heading",
+    description:
+      "This is a blog description will be given here as description blog description will be given here as description blog description will be given here as description",
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+];
